6c4946dfe2529c6a54d9ae77c7198254e3d5f957
Hyeon Kim <simnalamburt@gmail.com>
2015-10-03 02:42:56 +0900

    Make it simpler

--- api.rb
+++ api.rb
@@ -1,11 +1,11 @@
 require 'net/http'
 require 'json'
 
-def kollus_media_link_by_userid_api(api_token, security_key, media_content_key, media_profile_key, awt_code, client_user_id, expire_time = 7200, play_list = nil)
+def media_link(api_token, account_id, account_key, media_content_key, media_profile_key = nil, awt_code = nil, expire_time = 7200, play_list = nil)
   api_uri = URI('http://api.kr.kollus.com/0/media_auth/media_token/get_media_link_by_userid?access_token=' + api_token)
   params = {
-    'client_user_id' => client_user_id,
-    'security_key' => security_key,
+    'client_user_id' => account_id,
+    'security_key' => account_key,
     'media_content_key' => media_content_key,
     'media_profile_key' => media_profile_key,
     'awt_code' => awt_code,
